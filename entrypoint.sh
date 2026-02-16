c4b2ba84fe71d07eb87c627022165a24d1fe8992
root <root@git-0.git.default.svc.cluster.local>
2026-02-16 10:41:56 +0000

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@ set -e
 ROOT_LOGIN_UNLOCKED="true"
 ROOT_PASSWORD="root"
 
+ssh-keygen -A 1>/dev/null
+
 if [[ "${ROOT_LOGIN_UNLOCKED}" == "true" ]] ; then
     echo "root:${ROOT_PASSWORD}" | chpasswd &>/dev/null
     sed -i "s/#PermitRootLogin.*/PermitRootLogin\ yes/" /etc/ssh/sshd_config
