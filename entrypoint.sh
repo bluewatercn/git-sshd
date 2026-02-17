cb3a6f3fdc53a266595d5b5c3c423491d62b6f3a
root <root@git-0.git.default.svc.cluster.local>
2026-02-17 09:33:48 +0000

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,7 @@ if [[ "${ROOT_LOGIN_UNLOCKED}" == "true" ]] ; then
     echo "root:${ROOT_PASSWORD}" | chpasswd &>/dev/null
     sed -i "s/#PermitRootLogin.*/PermitRootLogin\ yes/" /etc/ssh/sshd_config
     sed -i 's/#ClientAliveInterval 0/ClientAliveInterval 20/g' /etc/ssh/sshd_config
+    sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 fi
 
 # do not detach (-D), log to stderr (-e), passthrough other arguments
