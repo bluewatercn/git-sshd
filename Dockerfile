5bb6b3b60ad0057503455618a6a9415a2fea3816
root <root@git-sshd-0.git-sshd.default.svc.cluster.local>
2026-02-18 06:47:33 +0000

    add auto git login

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM alpine:latest
 COPY entrypoint.sh /
 RUN chmod 777 /entrypoint.sh
-RUN apk update && apk upgrade && apk add --no-cache bash vim git openssh-client openssh-server
+RUN apk update && apk upgrade && apk add --no-cache curl bash vim git openssh-client openssh-server
+ENV TOKEN=''
+ENV ROOT_PASSWORD=''
+ENV SECRETPASSPHRASE=''
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,52 @@
 #!/usr/bin/env bash
 
-set -e
+set -eu
 
-
-ROOT_LOGIN_UNLOCKED="true"
-ROOT_PASSWORD="root"
+ROOT_PASSWORD=${ROOT_PASSWORD:-}
+TOKEN=${TOKEN:-}
+SECRETPASSPHRASE=${SECRETPASSPHRASE:-}
 
 ssh-keygen -A 1>/dev/null
 
-if [[ "${ROOT_LOGIN_UNLOCKED}" == "true" ]] ; then
-    echo "root:${ROOT_PASSWORD}" | chpasswd &>/dev/null
-    sed -i "s/#PermitRootLogin.*/PermitRootLogin\ yes/" /etc/ssh/sshd_config
-    sed -i 's/#ClientAliveInterval 0/ClientAliveInterval 20/g' /etc/ssh/sshd_config
-    sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+function token_exists {
+if [[ "$TOKEN" == ''  ]];then
+	echo "token must be set"
+	return 1
 fi
+}
+
+function rootpassword_exists {
+if [[ "$ROOT_PASSWORD" == ''  ]];then
+	echo "root password must be set"
+	return 1
+fi
+}
+
+function secretpassphrase_exists {
+if [[ "$SECRETPASSPHRASE" == ''  ]];then
+	echo "SECRETPASSPHRASE must be set"
+	return 1
+fi
+}
+
+
+rootpassword_exists
+token_exists
+secretpassphrase_exists
+
+#set root password login and heartbeat
+echo "root:${ROOT_PASSWORD}" | chpasswd &>/dev/null
+sed -i "s/#PermitRootLogin.*/PermitRootLogin\ yes/" /etc/ssh/sshd_config
+sed -i 's/#ClientAliveInterval 0/ClientAliveInterval 20/g' /etc/ssh/sshd_config
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+
+#set git token login
+if [[ ! -e /root/.ssh  ]];then
+	mkdir -p /root/.ssh
+fi
+ssh-keygen -t ed25519 -f /root/.ssh/id_ed25519 -N $SECRETPASSPHRASE -q
+
+
 
 # do not detach (-D), log to stderr (-e), passthrough other arguments
 exec /usr/sbin/sshd -D -e "$@"
